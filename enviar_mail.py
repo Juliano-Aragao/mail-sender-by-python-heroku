e1bc539304cf6ed5282ef405439dde5d1459a8f5
Julian <julianoesa@gmail.com>
2022-11-21 21:21:13 -0300

    aplicação para pegar dados de uma  API e enviar por e-mail

---
+++ enviar_mail.py
@@ -0,0 +1,25 @@
+import smtplib
+import email.message
+
+def enviar_email():  
+    corpo_email = """
+    <p>API Notificação</p>
+    <p>Python mail - by Julian</p>
+    """
+
+    msg = email.message.Message()
+    msg['Subject'] = "Acesso ao cadastro 2"
+    msg['From'] = 'julianoesa@gmail.com'
+    msg['To'] = 'julianoesa@gmail.com'
+    password = 'busylacolsauaode'
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email )
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    # Login Credentials for sending the mail
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print('Email enviado')
+    
+enviar_email()
